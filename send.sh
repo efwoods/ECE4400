3b472f7ac9e9edeba86f1d415d83b391793cf977
Tyler <tbernstein10@gmail.com>
2018-04-28 13:15:16 -0400

    Update

---
+++ send.sh
@@ -0,0 +1,31 @@
+#SSH ALIASES
+p1="ssh rrojas@pc3.instageni.stanford.edu -p 25011 -i ~/.ssh/id_geni2"
+p2="ssh rrojas@pc3.instageni.stanford.edu -p 25014 -i ~/.ssh/id_geni2"
+p3="ssh rrojas@pc3.instageni.stanford.edu -p 25015 -i ~/.ssh/id_geni2"
+p4="ssh rrojas@pc2.instageni.stanford.edu -p 25012 -i ~/.ssh/id_geni2"
+p5="ssh rrojas@pc3.instageni.stanford.edu -p 25016 -i ~/.ssh/id_geni2"
+p6="ssh rrojas@pc1.instageni.stanford.edu -p 25010 -i ~/.ssh/id_geni2"
+p7="ssh rrojas@pc3.instageni.stanford.edu -p 25017 -i ~/.ssh/id_geni2"
+p8="ssh rrojas@pc3.instageni.stanford.edu -p 25018 -i ~/.ssh/id_geni2"
+p9="ssh rrojas@pc3.instageni.stanford.edu -p 25019 -i ~/.ssh/id_geni2"
+p10="ssh rrojas@pc2.instageni.stanford.edu -p 25011 -i ~/.ssh/id_geni2"
+p11="ssh rrojas@pc3.instageni.stanford.edu -p 25012 -i ~/.ssh/id_geni2"
+p12="ssh rrojas@pc3.instageni.stanford.edu -p 25013 -i ~/.ssh/id_geni2"
+dv="ssh rrojas@pc3.instageni.stanford.edu -p 25010 -i ~/.ssh/id_geni2" 
+watson="ssh rrojas@pc2.instageni.stanford.edu -p 25013 -i ~/.ssh/id_geni2"
+dabo="ssh rrojas@pc2.instageni.stanford.edu -p 25010 -i ~/.ssh/id_geni2"
+
+#PASSWORD
+
+$p1 './go.sh' &
+$p2 './go.sh' &
+$p3 './go.sh' &
+$p4 './go.sh' &
+$p5 './go.sh' &
+$p6 './go.sh' &
+$p7 './go.sh' &
+$p8 './go.sh' &
+$p9 './go.sh' &
+$p10 './go.sh' &
+$p11 './go.sh' & 
+$p12 './go.sh' 
